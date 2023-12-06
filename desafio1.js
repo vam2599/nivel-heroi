82e9f3e488acf437d009d1c8064adcc4eef2bf71
vam2599 <128439765+vam2599@users.noreply.github.com>
2023-12-06 20:44:01 -0300

    Add files via upload

---
+++ desafio1.js
@@ -0,0 +1,21 @@
+let heroi = "Vinicius";
+let xp = 7500;
+
+if (xp < 1000) {
+    console.log ("O Herói de nome " + heroi + " esta no nível Ferro")
+} else if(xp <= 2000){
+    console.log ("O Herói de nome " + heroi + " esta no nível Bronze")
+} else if(xp <= 5000){
+    console.log ("O Herói de nome " + heroi + " esta no nível Prata")
+} else if(xp <= 7000){
+    console.log ("O Herói de noma " + heroi + " esta no nível Ouro")
+} else if(xp <= 8000){
+    console.log ("O Herói de nome " + heroi + " esta no nível Platina")
+} else if(xp <= 9000){
+    console.log ("O Herói de nome " + heroi + " esta no nível Ascendente")
+} else if(xp <= 10000){
+    console.log ("O Herói de nome " + heroi + " esta no nível Imortal")
+} else if(xp > 10000){
+    console.log ("O Herói de nome " + heroi + " esta no nível Radiante")
+} 
+
